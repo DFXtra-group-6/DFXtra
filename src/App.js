9a23529d062c2b968583be1d21742b1ef68dfeb8
Fran Bejarano <86791106+FranBejarano@users.noreply.github.com>
2023-03-16 15:51:09 +0000

    FeedbackComponent finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import ProfileBanner from "./components/ProfileBanner/profile-banner";
+import PersonalityType from "./components/Personality/PersonalityType";
+import FeedbackComponent from "./components/FeedbackComponent/feedbackComponent";
 
 function App() {
   return (
     <div>
       <ProfileBanner />
+      <PersonalityType />
+      <FeedbackComponent />
     </div>
   );
 }
