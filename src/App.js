3108f2f196551d0e129038ceb196aff1b16b7bd8
Pratik Pun <pratikpun@yahoo.com>
2023-03-16 16:17:43 +0000

    final commit with rendered components, moving to development branch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import NavbarComp from "./components/NavBar/NavbarComp";
 import Greeting from "./components/NavBar/Greeting";
 import PersonalityType from "./components/Personality/PersonalityType";
 import FeedbackComponent from "./components/FeedbackComponent/feedbackComponent";
+import Certifications from "./components/InfoBoxes/Certifications";
+import DueDiligence from "./components/InfoBoxes/DueDiligence";
+import Interests from "./components/InfoBoxes/Interests";
+import KeyTools from "./components/InfoBoxes/KeyTools";
+import Qualifications from "./components/InfoBoxes/Qualifications";
 
 function App() {
   return (
@@ -14,9 +19,13 @@ function App() {
       <Greeting />
       <ProfileBanner />
       <PersonalityType />
-      <FeedbackComponent />
+      {/* <Certifications /> */}
+      <DueDiligence />
+      {/* <Interests />
+      <KeyTools />
+      <Qualifications /> */}
       <AllExperience />
-      <PersonalityType />
+      <FeedbackComponent />
     </div>
   );
 }
