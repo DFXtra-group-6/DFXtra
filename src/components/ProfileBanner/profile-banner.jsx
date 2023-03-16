1ea02ab4b4acd989fc8caa48010fbcd2a4e2519f
Pratik Pun <pratikpun@yahoo.com>
2023-03-16 00:32:45 +0000

    made it responsive, need to add background

--- src/components/ProfileBanner/profile-banner.jsx
+++ src/components/ProfileBanner/profile-banner.jsx
@@ -5,8 +5,8 @@ import profileImg from "../../assets/profile-img.jpg";
 
 const ProfileBanner = () => {
   return (
-    <div className="container">
-      <div className="image-socials">
+    <div className=" container2">
+      <div className="image-socials ">
         <div className="user-image">
           <img src={profileImg} alt="default-profile" />
         </div>
@@ -36,10 +36,34 @@ const ProfileBanner = () => {
             voluptas repudiandae labore aliquam. Lorem, ipsum dolor sit amet
             consectetur adipisicing elit. Suscipit hic aut, sed officia, dolorum
             sint odit ea aliquam repellendus id dolor! Est mollitia sint amet
-            quod provident, error id minus?
+            quod provident, error id minus?Lorem ipsum, dolor sit amet
+            consectetur adipisicing elit. Accusamus quos qui odit atque?
+            Voluptatibus aperiam velit, eaque, ipsum, officiis alias nam eius
+            maxime laudantium dignissimos voluptate? Quisquam facere voluptates,
           </p>
         </div>
       </div>
+
+      {/* <div class="account-container">
+        <div class="card">
+          <div class="user">
+            <img
+              id="userAvatar"
+              src="https://images.pexels.com/photos/4587979/pexels-photo-4587979.jpeg"
+              width="100px"
+              height="100px"
+            />
+          </div>
+          <div class="accountStats">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+            inventore a animi rerum possimus. Numquam, recusandae velit.
+            Quibusdam placeat libero impedit deleniti laudantium tenetur maxime
+            officia omnis, voluptates pariatur ipsa totam provident cum,
+            inventore labore. Dolores illo ex minima, animi quia doloremque
+            quidem! Magni, mollitia laboriosam! Sed neque doloremque dicta?
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 };
