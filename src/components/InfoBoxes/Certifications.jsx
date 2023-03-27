ea95f2d13d292dbf751102882dc102a30f859a8f
ecorina <serban.corrina@gmail.com>
2023-03-27 16:44:32 +0100

    certification fix

--- src/components/InfoBoxes/Certifications.jsx
+++ src/components/InfoBoxes/Certifications.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 const Certifications = ({ certifications }) => {
   return (
-
     <div className="certi-grouped mt-3 border-rounded container bg-white">
       <h4>Certifications</h4>
       <div className="row bg-white">
         <div className="row border bg-light">
           <div className="icon col">
-            <img src={placeholder} className="icon" />
+            <img
+              src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/072/260/original/images.png?1679081214"
+              alt="icon"
+              className="icon"
+            />
           </div>
           <div className="description col">
             <p>Python</p>
@@ -16,14 +19,17 @@ const Certifications = ({ certifications }) => {
         </div>
         <div className="row border bg-light">
           <div className="icon col">
-            <img src={placeholder} className="icon" />
+            <img
+              src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/072/260/original/images.png?1679081214"
+              alt="icon"
+              className="icon"
+            />
           </div>
           <div className="description col">
             <p>javascript</p>
           </div>
         </div>
       </div>
-
     </div>
   );
 };
