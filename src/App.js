035ed3822fd9b53d23349658353ce2950bfc7562
Pratik Pun <pratikpun@yahoo.com>
2023-03-28 11:02:18 +0100

    added env for front-end, and modified app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import LoginPage from "./components/Login-page/login-page";
 import ProfilePage from "./components/ProfilePage/profilePage";
-import {BrowserRouter, Routes, Route}  from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/profile" element={<ProfilePage />}/>
-          <Route path="/" element={<LoginPage/>}/>
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/" element={<LoginPage />} />
         </Routes>
       </BrowserRouter>
     </div>
