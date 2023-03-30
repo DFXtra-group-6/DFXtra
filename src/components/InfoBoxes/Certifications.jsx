b4ee2ace804e211ed5389db927d4c5df64cbb9cf
nicolemuskett93@gmail.com <nicolemuskett93@gmail.com>
2023-03-30 15:11:02 +0100

    due diligence styling

--- src/components/InfoBoxes/Certifications.jsx
+++ src/components/InfoBoxes/Certifications.jsx
@@ -1,37 +1,34 @@
 import React from "react";
-import placeHolder from './images/certificate.svg';
-import './certifications.css';
+import placeHolder from "./images/certificate.svg";
+import "./certifications.css";
 
 const Certifications = ({ data }) => {
-    const certifications = data.certifications;
+  const certifications = data.certifications;
 
-    const populate = () => {
-        const display = certifications.map((certification, index) => {
-            return (
-                <div key={index} className="bg-white">
-                    <div className="row border bg-light">
-                        <div className="icon col">
-                            <img
-                                src={placeHolder}
-                                alt="icon"
-                                className="icon"
-                            />
-                        </div>
-                        <div className="description col">
-                            <p>{certification}</p>
-                        </div>
-                    </div>
-                </div>)
-        })
-        return display;
-    }
-
-    return (
-        <div className="certi-grouped mt-3 border-rounded container bg-white">
-            <h4>Certifications</h4>
-            {populate()}
+  const populate = () => {
+    const display = certifications.map((certification, index) => {
+      return (
+        <div key={index} className="bg-white">
+          <div className="row border bg-light">
+            <div className="icon col">
+              <img src={placeHolder} alt="icon" className="icon" />
+            </div>
+            <div className="description col">
+              <p>{certification}</p>
+            </div>
+          </div>
         </div>
-    );
+      );
+    });
+    return display;
+  };
+
+  return (
+    <div className="certi-grouped mt-3 border-rounded container bg-white">
+      <h4>Certifications</h4>
+      {populate()}
+    </div>
+  );
 };
 
 export default Certifications;
--- src/components/InfoBoxes/DueDiligence.jsx
+++ src/components/InfoBoxes/DueDiligence.jsx
@@ -1,40 +1,54 @@
 import React from "react";
-import tick from './images/tick.svg';
-import cross from './images/cross.svg';
-import './certifications.css';
+import tick from "./images/tick.svg";
+import cross from "./images/cross.svg";
+import "./dueDiligence.css";
+import creditImg from "../../assets/credit.png";
+import disclosureImg from "../../assets/disclosure.png";
+import educationImg from "../../assets/education.png";
+import identityImg from "../../assets/identity.png";
+import rightToWorkImg from "../../assets/rightToWork.png";
 
 const DueDiligence = ({ data }) => {
+  const { disclosure, identity, credit, rightToWork, education } =
+    data.dueDiligenceChecks;
 
-    const { disclosure, identity, credit, rightToWork, education } = data.dueDiligence;
+  function image(value) {
+    return value ? tick : cross;
+  }
 
-    function image(value) { return value ? tick : cross }
+  return (
+    <div className="border border-rounded bg-white mb-3">
+      <h4>Due Diligence Checks</h4>
+      {/* Just provides true/false at the moment */}
+      <div className="row due-row">
+        <img src={disclosureImg} className="col col-img"></img>
+        <p className="col">Disclosure</p>
+        <img src={image(disclosure)} className="icon col" />
+      </div>
 
-    return (
-        <div className="border border-rounded bg-white mb-3">
-            <h4>Due Diligence</h4>
-            {/* Just provides true/false at the moment */}
-            <div className="row m-1">
-                <img src={image(disclosure)} className="icon col" />
-                <p className="col">Disclosure</p>
-            </div>
-            <div className="row m-1">
-                <img src={image(identity)} className="icon col" />
-                <p className="col">Identity</p>
-            </div>
-            <div className="row m-1">
-                <img src={image(credit)} className="icon col" />
-                <p className="col">Credit</p>
-            </div>
-            <div className="row m-1">
-                <img src={image(rightToWork)} className="icon col" />
-                <p className="col">Right to Work</p>
-            </div>
-            <div className="row m-1">
-                <img src={image(education)} className="icon col" />
-                <p className="col">Education</p>
-            </div>
-        </div>
-    );
+      <div className="row due-row">
+        <img src={identityImg} className="col col-img"></img>
+        <p className="col">Identity</p>
+        <img src={image(identity)} className="icon col" />
+      </div>
+
+      <div className="row due-row">
+        <img src={creditImg} className="col col-img"></img>
+        <p className="col">Credit</p>
+        <img src={image(credit)} className="icon col" />
+      </div>
+      <div className="row due-row">
+        <img src={rightToWorkImg} className="col col-img"></img>
+        <p className="col">Right to Work</p>
+        <img src={image(rightToWork)} className="icon col" />
+      </div>
+      <div className="row due-row">
+        <img src={educationImg} className="col col-img"></img>
+        <p className="col">Education</p>
+        <img src={image(education)} className="icon col" />
+      </div>
+    </div>
+  );
 };
 
 export default DueDiligence;
