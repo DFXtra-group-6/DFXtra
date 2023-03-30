a13543c6ee8b2de0ff075b08843470bb096f2d7d
Fran Bejarano <86791106+FranBejarano@users.noreply.github.com>
2023-03-30 10:01:12 +0100

    add decision in the populate result

--- src/components/InfoBoxes/Qualifications.jsx
+++ src/components/InfoBoxes/Qualifications.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import placeholder from "../../assets/qualification-icon.png";
+import { useState, useEffect } from 'react'
 import './certifications.css';
 
 const Qualifications = ({ data }) => {
+
+    const [isFormVisible, setIsFormVisible] = useState(false);
 
     const handleEditButton = () => { }
 
@@ -12,38 +13,45 @@ const Qualifications = ({ data }) => {
         const display = qualifications.map(qualification => {
 
             return (
-                <div>
-                    <div className='bg-white'>
-                        <div className="row  border bg-light">
-                            <div className='icon col'>
-                                <i class="fa fa-certificate" aria-hidden="true"></i>
-                            </div>
-                            <div className='description col-8'>
-                                <p>{qualification.level} | {qualification.description}</p>
-                                <p>{qualification.institution}</p>
+                <>
+                    {(qualifications.length <= 0) ? (
+
+                        <div className="text-center border m-4 bg-light">
+                            "No qualification added"
+                        </div>
+
+                    ) : (< div >
+                        <div className='bg-white'>
+                            <div className='row  m-1'>
+                                <div className='icon col'>
+                                    <i class="fa fa-certificate" aria-hidden="true"></i>
+                                </div>
+                                <div className='description col-8'>
+                                    <p>{qualification.level} | {qualification.description}</p>
+                                    <p>{qualification.institution}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    </div >)}
+                </>
             )
-        })
+        }
+
+        )
         return display;
     }
-
     return (
-        <div className="certi-grouped mt-3 border-rounded container bg-white">
-            <div className="row space-between">
-                <div className="col">
-                    <h4>Qualifications</h4>
-                </div>
-                <div className="col">
-                    <i className="fa fa-light fa-pencil" onClick={handleEditButton}></i>
-                </div>
+        <div className="border border-rounded bg-white mb-3">
+            <div className="d-flex justify-content-between align-items-center m-1">
+                <h4>Qualifications</h4>
+                <i className="fa fa-light fa-pencil" onClick={handleEditButton} disabled={true}></i>
             </div>
             {populate()}
         </div>
     )
 
 }
+
+
 
 export default Qualifications
